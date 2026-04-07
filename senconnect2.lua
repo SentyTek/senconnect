03ddf2c2b06e95512e42a43c1c1a2d1c9e7c3b97
SentyFunball <sentyfb@gmail.com>
2026-04-07 00:11:24 -0700

    auto transmitter frequency selection based on avail freqs.

--- senconnect2.lua
+++ senconnect2.lua
@@ -45,11 +45,14 @@ end
 -- try require("Folder.Filename") to include code from another file in this, so you can store code in libraries
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
 
-local lb = require("LifeBoatAPI")
-
 local groupID = property.getNumber("Group ID")
 local refreshInterval = property.getNumber("Refresh Interval (sec)") * 60
 local playerTimeout = property.getNumber("Player Timeout (num refreshes)")
+
+local scannerFreq = 100
+local maxFreq = 200
+local transmitterFreq = 0
+local ready = false
 
 local mapColors = { -- From SenCar
     { 47, 51, 78 },
@@ -69,9 +72,70 @@ local mapProperties = {
     color = mapColors[1]
 }
 
+local playerFreqs = {}
+local startupFoundFreqs = {}
+
+local rollingScannerFreqs = {}
+
+local function contains(t, v)
+    for _, value in pairs(t) do
+        if value == v then return true end
+    end
+    return false
+end
+
+local ticks = 0
+
 function onTick()
-    local enabled = not input.getBool(1)
-    if not enabled then return end
+    if ticks < 5 then
+        ticks = ticks + 1
+    end
+    local enabled = not input.getBool(2)
+
+    -- select frequency if not ready by scanning through all frequencies,
+    -- noting whats taken, and then picking an open one once the scanner
+    -- has gone through them all
+    if ticks >= 5 and not ready and enabled then
+        if input.getBool(1) then -- found
+            local datedScannerFreq = rollingScannerFreqs[1] or 100
+            local f = datedScannerFreq
+            if f ~= 0 and not contains(startupFoundFreqs, f) then
+                startupFoundFreqs[#startupFoundFreqs + 1] = f
+            end
+        end
+        scannerFreq = scannerFreq + 1
+
+        if scannerFreq >= 200 then
+            -- select from the open frequencies
+            -- min freq is 100, max is 200
+            local freq = 100
+            while freq <= maxFreq do
+                if not contains(startupFoundFreqs, freq) then
+                    break
+                end
+                freq = freq + 1
+            end
+
+            transmitterFreq = freq
+            ready = true
+        end
+    end
+
+    -- map inputs and such
+    output.setNumber(1, #startupFoundFreqs)
+    output.setNumber(28, scannerFreq)
+    output.setNumber(29, transmitterFreq)
+    output.setBool(1, ready)
+    rollingScannerFreqs[#rollingScannerFreqs + 1] = scannerFreq
+    if #rollingScannerFreqs > 6 then table.remove(rollingScannerFreqs, 1) end
+    if not enabled then
+        scannerFreq = 100
+        transmitterFreq = 0
+        ready = false
+        startupFoundFreqs = {}
+        playerFreqs = {}
+        return
+    end
 
     local zoom = input.getNumber(31)
     if zoom ~= 0 then mapProperties.zoom = zoom else mapProperties.zoom = 1 end
@@ -83,13 +147,25 @@ function onTick()
         mapProperties.y = py
         mapProperties.heading = ph
     end
+
+    -- get inputs from the receiver
+    -- important to note the receiver is scanning frequencies set several ticks ago (5 tick to be precise)
+    local alive = input.getBool(1)
+    local rgroupID = input.getNumber(1)
+    if alive and rgroupID == groupID then
+        playerFreqs[#playerFreqs + 1] = rollingScannerFreqs[1]
+    end
 end
 
 function onDraw()
     local w, h = screen.getWidth(), screen.getHeight()
     screen.drawMap(mapProperties.x, mapProperties.y, mapProperties.zoom)
     screen.setColor(mapProperties.color[1], mapProperties.color[2], mapProperties.color[3])
-    drawPointer(w/2, h/2, 8, mapProperties.heading)
+    drawPointer(w / 2, h / 2, 8, mapProperties.heading)
+    
+    for _, f in pairs(startupFoundFreqs) do
+        screen.drawText(0, _ * 7, f)
+    end
 end
 
 function drawPointer(x,y,s,r,...)
