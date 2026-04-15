ea49cfb04e2aca5d634ae5deb638e2ccd5e7d618
SentyFunball <sentyfb@gmail.com>
2026-04-14 21:43:27 -0700

    ability to transmit text over the radio, allow touch to show text

--- senconnect2.lua
+++ senconnect2.lua
@@ -15,15 +15,20 @@ do
     simulator = simulator
     simulator:setScreen(1, "2x2")
     simulator:setScreen(2, "3x1")
-    
+
     simulator:setProperty("Group ID", 1522)
     simulator:setProperty("Refresh Interval (sec)", 3)
     simulator:setProperty("Player Timeout (num refreshes)", 2)
+    simulator:setProperty("Unit Name", "Aita")
 
     -- Runs every tick just before onTick; allows you to simulate the inputs changing
     ---@param simulator Simulator Use simulator:<function>() to set inputs etc.
     ---@param ticks     number Number of ticks since simulator started
     function onLBSimulatorTick(simulator, ticks)
+
+        s = simulator:getTouchScreen(1)
+        simulator:setInputNumber(26, s.touchX) -- touch x
+        simulator:setInputNumber(27, s.touchY) -- touch y
 
         -- map settings
         simulator:setInputNumber(28, 0) -- x
@@ -39,6 +44,13 @@ do
             simulator:setInputBool(1, true) -- alive
             simulator:setInputNumber(1, 1522) -- group ID
             simulator:setInputNumber(2, 150 + math.floor(simulator:getSlider(3) * 50)) -- transmitter freq
+            simulator:setInputNumber(3, math.sin(ticks / 20) * 100) -- x
+            simulator:setInputNumber(4, math.cos(ticks / 20) * 100)                    -- y
+            simulator:setInputNumber(5, (ticks / 20) % (math.pi * 2))                    -- heading
+            simulator:setInputNumber(6, math.floor(simulator:getSlider(4) * 7))        -- colorIndex
+            -- simulate name "BOB" being sent over
+            simulator:setInputNumber(7, 66 * 256 + 79) -- "BO" in bytes
+            simulator:setInputNumber(8, 66 * 256) -- "B" in bytes
         else
             simulator:setInputBool(1, false) -- alive
             simulator:setInputNumber(1, 0) -- group ID
@@ -57,6 +69,15 @@ end
 local groupID = property.getNumber("Group ID")
 local refreshInterval = math.max(60, property.getNumber("Refresh Interval (sec)") * 60)
 local playerTimeout = math.max(0, math.floor(property.getNumber("Player Timeout (num refreshes)")))
+local unitName = property.getText("Unit Name"):upper()
+
+local bytes = {}
+local startChannelName = 7
+for i = 1, math.min(#unitName, 36) do
+    local char = unitName:sub(i, i)
+    local byte = string.byte(char)
+    bytes[#bytes + 1] = byte
+end
 
 local startingFreq = 95
 local scannerFreq = startingFreq
@@ -111,6 +132,39 @@ end
 
 local ticks = 0
 local state = 0 -- 0 = unready 1 = starting 2 = ready 3 = scanning for players 4 = looping players
+local touchX, touchY, ltouchX, ltouchY, ticksSinceTouch = 0, 0, 0, 0, 0
+
+local function txName()
+    -- translates string to bytes and sends over outputs
+    -- since we only use values (30..39, 65..90) we can send 2 chars per channel
+    for i = 1, #bytes, 2 do
+        local byte1 = bytes[i] or 0
+        local byte2 = bytes[i + 1] or 0
+        local combined = byte1 * 256 + byte2
+        output.setNumber(startChannelName + math.floor((i - 1) / 2), combined)
+    end
+end
+
+local function rxName()
+    -- receives bytes from inputs and translates back to string
+    local receivedBytes = {}
+    for i = startChannelName, 25 do
+        local combined = input.getNumber(i)
+        if combined == 0 then goto continue end
+        local byte1 = math.floor(combined / 256)
+        local byte2 = combined % 256
+        if byte1 ~= 0 then table.insert(receivedBytes, byte1) end
+        if byte2 ~= 0 then table.insert(receivedBytes, byte2) end
+        ::continue::
+    end
+
+    local chars = {}
+    for _, byte in pairs(receivedBytes) do
+        table.insert(chars, string.char(byte))
+    end
+
+    return table.concat(chars):upper()
+end
 
 function onTick()
     ticks = ticks + 1
@@ -136,7 +190,7 @@ function onTick()
             while contains(startupFoundFreqs, freq) do
                 freq = math.random(startingFreq, maxFreq)
             end
-            
+
             transmitterFreq = freq
             state = 2 -- ready
             scannerFreq = startingFreq -- Go a few lower to give some time before the first scan starts at 100
@@ -169,22 +223,24 @@ function onTick()
         mapProperties.heading = ph
     end
 
+    touchX, touchY = input.getNumber(26), input.getNumber(27)
+
     -- loop through all frequencies to see where players exist and note their transmitter freqs
     if state == 2 or state == 3 then
         playerData = {}
         currentPlayerIndex = 1
         state = 3 -- scanning for players
-        
+
         local alive = input.getBool(1)
         local incomingTransmitterFreq = input.getNumber(2)
         if alive and incomingTransmitterFreq ~= transmitterFreq then
             local rgroupID = input.getNumber(1)
-            
+
             if rgroupID == groupID and not contains(playerFreqs, incomingTransmitterFreq) then
                 playerFreqs[#playerFreqs + 1] = incomingTransmitterFreq -- transmitter freq of the found signal
             end
         end
-        
+
         scannerFreq = scannerFreq + 1
         if scannerFreq >= 206 then
             local detectedThisRefresh = {}
@@ -232,18 +288,21 @@ function onTick()
             local playerFreq = playerFreqs[playerIndex]
             scannerFreq = playerFreq
 
-            if input.getBool(1) then
+            local alive = input.getBool(1)
+            if alive then
                 trackedPlayers[playerFreq] = trackedPlayers[playerFreq] or { missedRefreshes = 0 }
                 trackedPlayers[playerFreq].x = input.getNumber(3)
                 trackedPlayers[playerFreq].y = input.getNumber(4)
                 trackedPlayers[playerFreq].heading = input.getNumber(5)
                 trackedPlayers[playerFreq].color = input.getNumber(6)
+                trackedPlayers[playerFreq].name = rxName()
 
                 playerData[playerIndex] = {
                     x = input.getNumber(3),
                     y = input.getNumber(4),
                     heading = input.getNumber(5),
-                    color = input.getNumber(6)
+                    color = input.getNumber(6),
+                    name = rxName()
                 }
             end
 
@@ -254,7 +313,6 @@ function onTick()
             end
         end
     end
-
 
     -- local outputs
     output.setNumber(28, scannerFreq)
@@ -267,11 +325,13 @@ function onTick()
     output.setNumber(4, mapProperties.y)
     output.setNumber(5, mapProperties.heading)
     output.setNumber(6, mapProperties.colorIndex or 1)
-    
+
+    txName()
+
     output.setBool(1, state > 1)
 
-    output.setNumber(31, state)
-    output.setNumber(32, #playerData)
+    ticksSinceTouch = (touchX ~= ltouchX or touchY ~= ltouchY) and 0 or ticksSinceTouch + 1
+    ltouchX, ltouchY = touchX, touchY
 
     if scannerFreq > 300 then
         scannerFreq = startingFreq
@@ -279,12 +339,13 @@ function onTick()
 end
 
 function onDraw()
+    local nameToDisplay = { x = 0, y = 0, name = "" }
     local w, h = screen.getWidth(), screen.getHeight()
     screen.drawMap(mapProperties.x, mapProperties.y, mapProperties.zoom)
 
     -- draw other players first so our pointer gets overlaid on top
     for _, player in pairs(mapPlayerData) do
-        if player ~= nil and player.x ~= nil and player.y ~= nil and player.heading ~= nil and player.color ~= nil then
+        if player and player.x then
             local playerColor = mapColors[toColorIndex(player.color)] or mapColors[1]
             c(
                 playerColor[1],
@@ -293,11 +354,24 @@ function onDraw()
             )
             local px, py = map.mapToScreen(mapProperties.x, mapProperties.y, mapProperties.zoom, w, h, player.x, player.y)
             drawPointer(px, py, 6, player.heading)
+
+            -- create small touch zones for each player to display their name on the screen when touched
+            if ticksSinceTouch < 300 and isPointInRectangle(px - 2, py - 4, 4, 4) then
+                nameToDisplay = { x = px + 2, y = py - 4, name = player.name or "" }
+            end
         end
     end
 
     c(mapProperties.color[1], mapProperties.color[2], mapProperties.color[3])
     drawPointer(w / 2, h / 2, 8, mapProperties.heading)
+    c(255, 255, 255)
+
+    if nameToDisplay.name ~= "" then
+        c(50, 50, 50, 200)
+        screen.drawRectF(nameToDisplay.x, nameToDisplay.y, #nameToDisplay.name * 4 + 1, 7)
+        c(255, 255, 255)
+        dst(nameToDisplay.x + 1, nameToDisplay.y + 1, nameToDisplay.name or "")
+    end
 end
 
 function drawPointer(x,y,s,r,...)
@@ -315,4 +389,40 @@ function c(...)
         _[i] = _[i] ^ 2.2 / 255 ^ 2.2 * _[i]
     end
     screen.setColor(table.unpack(_))
+end
+
+function isPointInRectangle(rectX, rectY, rectW, rectH)
+    return touchX >= rectX and touchY >= rectY and touchX <= rectX + rectW and touchY <= rectY + rectH
+end
+
+--dst(x,y,text,size=1,rotation=1,is_monospace=false)
+--rotation can be between 1 and 4
+f=screen.drawRectF
+--magic willy font
+h="00019209B400AAAA793CA54A555690015244449415500BA0004903800009254956D4592EC54EC51C53A4F31C5354E52455545594104110490A201C7008A04504FFFE57DAD75C7246D6DCF34EF3487256B7DAE92E64D4975A924EBEDAF6DAF6DED74856B2D75A711CE924B6D4B6A4B6FAB55AB524E54ED24C911264965400000E"
+i={}j=0
+for k in h:gmatch("....")do i[j+1]=tonumber(k,16)j=j+1 end
+function dst(l, m, n, b, o, p)
+    b = b or 1
+    o = o or 1
+    if o > 2 then n = n:reverse() end
+    n = n:upper()
+    for q in n:gmatch(".") do
+        r = q:byte() - 31
+        if 0 < r and r <= j then
+            for s = 1, 15 do
+                if o > 2 then t = 2 ^ s else t = 2 ^ (16 - s) end
+                if i[r] & t == t then
+                    u, v = ((s - 1) % 3) * b, ((s - 1) // 3) * b
+                    if o % 2 == 1 then f(l + u, m + v, b, b) else f(l + 5 - v, m + u, b, b) end
+                end
+            end
+            if i[r] & 1 == 1 and not p then
+                s = 2 * b
+            else
+                s = 4 * b
+            end
+            if o % 2 == 1 then l = l + s else m = m + s end
+        end
+    end
 end
